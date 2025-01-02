b767b019b023d6160b8a26e32c3d5a3c02abef2a
Charity Rymbai <charityrymbai@hotmail.com>
2025-01-02 14:54:38 +0530

    initial commit

---
+++ web/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import fs from 'fs'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: {
+      key: fs.readFileSync('../private-key.pem'),
+      cert: fs.readFileSync('../certificate.pem'),
+    },
+    host: '0.0.0.0',
+  },
+})
